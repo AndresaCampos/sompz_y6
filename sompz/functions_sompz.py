c3c1b79e47fca9affa88780b1534cdcada28f2c6
AndresaCampos <acampos@cmu.edu>
2023-03-09 15:01:32 -0500

    fixed warning pcchat

--- sompz/functions_sompz.py
+++ sompz/functions_sompz.py
@@ -55,7 +55,7 @@ def calculate_weights(smooth_response_file, snr, size_ratio, injection_counts, u
 def calculate_pcchat(deep_som_size, wide_som_size, cell_deep_assign, cell_wide_assign, overlap_weight):
     pcchat_num = np.zeros((deep_som_size, wide_som_size))
     np.add.at(pcchat_num,
-              [cell_deep_assign, cell_wide_assign],
+              [tuple(cell_deep_assign), tuple(cell_wide_assign)],
               overlap_weight)
 
     pcchat_denom = pcchat_num.sum(axis=0)
