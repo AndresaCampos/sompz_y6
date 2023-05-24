e0f5992fa0cd37611a7c490760c0e185dc4f6410
AndresaCampos <acampos@cmu.edu>
2023-05-24 17:03:36 -0400

    y3 assign sh files

---
+++ assign_balrog_wide.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --partition=debug
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=256
+#SBATCH --constraint=haswell
+#SBATCH --time=30:00
+#SBATCH --license=SCRATCH
+#SBATCH --output=JOB/JOB_OUT_check_%j.txt
+#SBATCH --error=JOB/JOB_ERR_check_%j.txt
+#SBATCH --mail-user acampos@cmu.edu
+#SBATCH --mail-type BEGIN
+#SBATCH --mail-type END
+
+set -x
+
+export HDF5_USE_FILE_LOCKING='FALSE'
+
+conda activate sompz
+
+srun -n 256 python assign_SOM_wide_balrog_mpi.py
