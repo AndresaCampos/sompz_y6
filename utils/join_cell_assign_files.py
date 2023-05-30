9a1f6d42091fab9dabf80582f7ced612325f05e9
AndresaCampos <acampos@cmu.edu>
2023-05-30 17:08:56 -0400

    updated util script to join

--- utils/join_cell_assign_files.py
+++ utils/join_cell_assign_files.py
@@ -1,53 +1,38 @@
 import h5py
+import pickle
 import numpy as np
 
 
-# def join_cell_assign_files(path_cats, som_type, shear):
-#     cells = []
-#     for item in range(256):
-#         for subsample in range(10):
-#             cells_subsample = np.load(f'{path_cats}/{som_type}_{shear}/som_wide_32x32_1e7_assign_{som_type}_{shear}_{item}_subsample_{subsample}.npz')['cells']
-#             cells.append(cells_subsample)
-
-#     cells = np.concatenate(cells)
-#     np.savetxt(f'cells_{som_type}_{shear}_newSOM_y3.txt', cells)
-#     print(f'done with {shear}')
-
-def join_cell_assign_files(path_cats, som_type, shear):
+def join_cell_assign_files(path_cats, som_type, shear, run_name):
     cells = []
     for item in range(256):
         for subsample in range(10):
-            cells_subsample = np.load(
-                f'{path_cats}/{som_type}_{shear}/som_wide_32x32_1e7_assign_{som_type}_{shear}_{item}_subsample_{subsample}.npz')[
-                'cells']
+            if som_type == 'wide':
+                cells_subsample = np.load(
+                    f'{path_cats}/{som_type}_{shear}/som_wide_32_32_1e7_assign_{shear}_{item}_subsample_{subsample}.npz')[
+                    'cells']
+            else:
+                cells_subsample = np.load(
+                    f'{path_cats}/{som_type}_{shear}/som_deep_assign_{shear}_{item}_subsample_{subsample}.npz')[
+                    'cells']
             cells.append(cells_subsample)
-
     cells = np.concatenate(cells)
-    with h5py.File('sompz.hdf5', 'w', track_order=True) as f:
-        f.create_dataset(f'catalog/sompz/{shear}/cell_wide', data=cells)
-    print(f'done with {shear}')
+    
+    with open(f"path_cats/cells_{som_type}_{shear}_{run_name}.pkl", "wb") as output_file:
+        pickle.dump(cells, output_file)
+    
+    # with h5py.File('sompz.hdf5', 'w', track_order=True) as f:
+    #     f.create_dataset(f'catalog/sompz/{shear}/cell_wide', data=cells)
+    # print(f'done with {shear}')
 
 
-def join_cell_assign_files_unsheared(path_cats, som_type):
-    cells = []
-    for item in range(128):
-        for subsample in range(10):
-            cells_subsample = \
-            np.load(f'{path_cats}/{som_type}_unsheared/som_wide_32x32_1e7_assign_{item}_subsample_{subsample}.npz')[
-                'cells']
-            cells.append(cells_subsample)
+path_cats = '/global/cfs/projectdirs/des/acampos/sompz_output/y6_data_preliminary'
+run_name = 'y6preliminary'
 
-    cells = np.concatenate(cells)
-    with h5py.File('sompz.hdf5', 'w', track_order=True) as f:
-        f.create_dataset(f'catalog/sompz/unsheared/cell_wide', data=cells)
-    print(f'done with unsheared')
+# join_cell_assign_files(path_cats, som_type, 'sheared_1m')
+# join_cell_assign_files(path_cats, som_type, 'sheared_1p')
+# join_cell_assign_files(path_cats, som_type, 'sheared_2m')
+# join_cell_assign_files(path_cats, som_type, 'sheared_2p')
 
-
-som_type = 'wide'
-path_cats = '/global/cscratch1/sd/acampos/sompz/test/full_run_on_data/SOM/cats_Y3'
-
-join_cell_assign_files(path_cats, som_type, 'sheared_1m')
-join_cell_assign_files(path_cats, som_type, 'sheared_1p')
-join_cell_assign_files(path_cats, som_type, 'sheared_2m')
-join_cell_assign_files(path_cats, som_type, 'sheared_2p')
-join_cell_assign_files_unsheared(path_cats, som_type)
+join_cell_assign_files(path_cats, 'deep', 'deep_balrog',run_name)
+join_cell_assign_files(path_cats, 'wide', 'deep_balrog',run_name)
